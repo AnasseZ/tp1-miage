27e6552cf340962dca5d81eacf064fc6e17e3cd6
Anasse Zougarh <zougarh.anasse@gmail.com>
2018-03-22 15:10:13 +0100

    Refacto finished

--- ex1-ini/mailreader/src/main/java/com/acme/mailreader/utils/MailComparator.java
+++ ex1-ini/mailreader/src/main/java/com/acme/mailreader/utils/MailComparator.java
@@ -12,31 +12,66 @@ import com.acme.mailreader.model.Mail;
  */
 public class MailComparator implements Comparator<Mail> {
 	
-	public final static int IMPOSSIBLE_COMPARAISON = 0;
+	public final static int EQUALS = 0;
 	public final static int MAIL_MORE_IMPORTANT = -1;
 	public final static int OTHER_MORE_IMPORTANT = 1;
 	
 	public int compare(Mail mail, Mail other) {
-		if (mail == null || other == null) {
-			return IMPOSSIBLE_COMPARAISON;
+		if (oneOfMailsNull(mail, other)) {
+			return EQUALS;
 		}
-		if (mail.isImportant() != other.isImportant()) {
-			if (mail.isImportant() && !other.isImportant()) {
-				return MAIL_MORE_IMPORTANT;
-			} 
-			
-			return OTHER_MORE_IMPORTANT;		
+		if (differentImportance(mail, other)) {
+			return sortByImportance(mail, other);
 		}
-		if (mail.getStatut() != other.getStatut()) {
-			int comp = mail.getStatut().ordinal()
-					- other.getStatut().ordinal();
-			return comp > IMPOSSIBLE_COMPARAISON ? MAIL_MORE_IMPORTANT : OTHER_MORE_IMPORTANT;
+		if (differentStatut(mail, other)) {
+			return sortByStatut(mail, other);
 		}
-		if (mail.getSujet() != other.getSujet()) {
-			return other.getSujet().compareTo(mail.getSujet());
+		if (differentSujet(mail, other)) {
+			return sortBySujet(mail, other);
 		}
-		return other.getDate().compareTo(mail.getDate());
+		return mail.getDate().compareTo(other.getDate());
 	}
-	
+
+	private int sortByImportance(Mail mail, Mail other) {
+		if (mail.isImportant() && !other.isImportant()) {
+			return MAIL_MORE_IMPORTANT;
+		} 
+		
+		return OTHER_MORE_IMPORTANT;
+	}
+
+	private int sortByStatut(Mail mail, Mail other) {
+		int comp = mail.getStatut().ordinal() - other.getStatut().ordinal();
+		return comp < 0 ? OTHER_MORE_IMPORTANT : MAIL_MORE_IMPORTANT;
+	}
+
+	private int sortBySujet(Mail mail, Mail other) {
+		if (mail.getSujet() == null && other.getSujet() != null) {
+			return OTHER_MORE_IMPORTANT;
+		}
+		else if (mail.getSujet() != null && other.getSujet() == null) {
+			return MAIL_MORE_IMPORTANT;
+		}
+		return mail.getSujet().compareTo(other.getSujet());
+	}
+
+	private boolean oneOfMailsNull(Mail mail, Mail other) {
+		return mail == null || other == null;
+	}
+
+	private boolean differentImportance(Mail mail, Mail other) {
+		return mail.isImportant() != other.isImportant();
+	}
+
+	private boolean differentStatut(Mail mail, Mail other) {
+		return mail.getStatut() != other.getStatut();
+	}
+
+	private boolean differentSujet(Mail mail, Mail other) {
+		if (mail.getSujet() == null || other.getSujet() == null) {
+			return true;
+		}
+		return !mail.getSujet().equals(other.getSujet());
+	}
 
 }
